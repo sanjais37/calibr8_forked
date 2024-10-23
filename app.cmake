80bc0f1cefb0a3d9f82684529d171f5d7153e616
Sanjeev Kumar <sanjeev.kumar@austin.utexas.edu>
2024-10-22 19:02:11 -0500

    Adding gitignore file

--- app.cmake
+++
@@ -1,9 +0,0 @@
-capp_app(
-  BUILD_TYPE
-  ${CALIBR8_BUILD_TYPE}
-  ROOT_PACKAGES
-  calibr8
-)
-
-find_program(CALIBR8_MPICC NAMES mpicc HINTS ENV MPICC)
-find_program(CALIBR8_MPICXX NAMES mpicxx HINTS ENV MPICXX)
